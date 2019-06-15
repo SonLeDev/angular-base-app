0f5dbdcce798556521462d796818010c779205b6
SonLe <freetrend.it.sonle@gmail.com>
2019-06-16 00:22:58 +0700

    cang import

--- src/app/form-base/components/formbuilder/detail/detail.component.ts
+++ src/app/form-base/components/formbuilder/detail/detail.component.ts
@@ -1,54 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IForm } from 'src/app/form-base/interfaces/IForm';
-import { FormBuilder } from 'src/app/form-base/models/FormBuilder';
+import { Component, OnInit, Input } from "@angular/core";
+import { IForm } from "../../../interfaces/IForm";
 
 @Component({
-	selector: 'app-formbuilder-detail',
-	templateUrl: './detail.component.html',
-	styleUrls: ['./detail.component.css']
+  selector: "app-formbuilder-detail",
+  templateUrl: "./detail.component.html",
+  styleUrls: ["./detail.component.css"]
 })
 export class DetailFormbuilderComponent implements OnInit {
+  readForm: IForm;
+  editForm: IForm;
+  private readOnly: boolean = true;
 
-	readForm: IForm;
-	editForm: IForm;
-	private readOnly: boolean = true;
+  @Input() set form(value: IForm) {
+    this.readForm = Object.assign({}, value);
+    this.editForm = Object.assign({}, value);
+  }
 
-	@Input() set form(value: IForm) {
+  constructor() {
+    this.readOnly = true;
+    console.log("[DetailFormbuilderComponent::constructor] : form : ");
+  }
 
-		this.readForm = Object.assign({}, value);
-		this.editForm = Object.assign({}, value);
-	}
+  onSaveState() {}
 
-	constructor() {
-		this.readOnly = true;
-		console.log("[DetailFormbuilderComponent::constructor] : form : ");
+  onEditState() {
+    this.readOnly = false;
+  }
+  onCancelState() {
+    this.readOnly = true;
+    //restore original form if cancel
+    this.editForm = Object.assign({}, this.readForm);
+  }
 
-	}
+  canEdit() {
+    let retVal = false;
+    if (!this.readOnly && this.readForm && this.editForm) {
+      retVal = true;
+    }
+    return retVal;
+  }
 
-	onSaveState() {
-
-	}
-
-	onEditState() {
-		this.readOnly = false;
-	}
-	onCancelState() {
-		this.readOnly = true;
-		//restore original form if cancel
-		this.editForm = Object.assign({}, this.readForm);
-	}
-
-	canEdit() {
-		let retVal = false;
-		if (!this.readOnly && this.readForm && this.editForm) {
-			retVal = true;
-		}
-		return retVal;
-	}
-
-
-
-	ngOnInit() {
-	}
-
+  ngOnInit() {}
 }
--- src/app/form-base/components/formbuilder/formbuilder.component.ts
+++ src/app/form-base/components/formbuilder/formbuilder.component.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { IForm } from '../../interfaces/IForm';
-import { FormBuilder } from '../../models/FormBuilder';
+import { Component, OnInit } from "@angular/core";
+import { IForm } from "../../interfaces/IForm";
+import { FormBuilder } from "../../models/FormBuilder";
 
 @Component({
-	selector: 'app-formbuilder',
-	templateUrl: './formbuilder.component.html',
-	styleUrls: ['./formbuilder.component.css']
+  selector: "app-formbuilder",
+  templateUrl: "./formbuilder.component.html",
+  styleUrls: ["./formbuilder.component.css"]
 })
 export class FormbuilderComponent implements OnInit {
+  forms: Array<IForm> = [];
+  currSelectedForm: IForm;
 
-	forms: Array<IForm> = [];
-	currSelectedForm: IForm;
+  viewFormDetail(onSelectedRow: IForm) {
+    this.currSelectedForm = onSelectedRow;
+    console.log(
+      "[FormbuilderComponent::viewFormDetail] : currSelectedForm : ",
+      JSON.stringify(this.currSelectedForm)
+    );
+  }
 
-	viewFormDetail(onSelectedRow: IForm) {
-		this.currSelectedForm = onSelectedRow;
-		console.log("[FormbuilderComponent::viewFormDetail] : currSelectedForm : ", JSON.stringify(this.currSelectedForm));
-	}
+  submitIForm(form: IForm) {
+    console.log(
+      "[FormbuilderComponent::submitIForm] : form : ",
+      JSON.stringify(form)
+    );
+  }
 
-	submitIForm(form: IForm) {
-		console.log("[FormbuilderComponent::submitIForm] : form : ", JSON.stringify(form));
-
-	}
-
-	ngOnInit() {
-		//TODO : should get somewhere in store-redux
-		this.forms.push(new FormBuilder('Employee'));
-		this.forms.push(new FormBuilder('Department'));
-		this.forms.push(new FormBuilder('Address'));
-	}
-
+  ngOnInit() {
+    //TODO : should get somewhere in store-redux
+    this.forms.push(new FormBuilder("Employee"));
+    this.forms.push(new FormBuilder("Department"));
+    this.forms.push(new FormBuilder("Address"));
+  }
 }
